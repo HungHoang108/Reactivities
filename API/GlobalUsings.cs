9f7bdcaa06ffd82b802439c7df2ecce31ac6553d
hung hoang <hung.xamk@gmail.com>
2023-02-23 20:53:46 +0200

    add crud operation to activity

---
+++ API/GlobalUsings.cs
@@ -0,0 +1,4 @@
+// global using MediatR;
+// global using Microsoft.EntityFrameworkCore;
+// global using Microsoft.OpenApi.Models;
+// global using Persistence;
--- API/Program.cs
+++ API/Program.cs
@@ -1,17 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Application.Activities;
-using MediatR;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Authorization;
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,57 +7,35 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-});
+builder.Services.AddApplicationServices(builder.Configuration);
 
-builder.Services.AddDbContext<DataContext>(opt =>
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
 {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
 }
-);
+app.UseCors("CorsPolicy");
 
-builder.Services.AddCors(opt =>
+app.UseAuthorization();
+
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+try
 {
-    opt.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-    });
-});
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "Error during migration");
+}
 
-builder.Services.AddMediatR(typeof(List.Handler).Assembly);
+await app.RunAsync();
 
-// namespace API
-// {
-//     public class Program
-//     {
-//         public static async Task Main(string[] args)
-//         {
-//             var host = CreateHostBuilder(args).Build();
-//             using var scope = host.Services.CreateScope();
-
-//             var services = scope.ServiceProvider;
-//             try
-//             {
-//                 var context = services.GetRequiredService<DataContext>();
-//                 await context.Database.MigrateAsync();
-//                 await Seed.SeedData(context);
-//             }
-//             catch (Exception ex)
-//             {
-//                 var logger = services.GetRequiredService<ILogger<Program>>();
-//                 logger.LogError(ex, "Error during migration");
-
-//             }
-//             await host.RunAsync();
-//         }
-
-//         public static IHostBuilder CreateHostBuilder(string[] args) =>
-//             Host.CreateDefaultBuilder(args)
-//                 .ConfigureWebHostDefaults(webBuilder =>
-//                 {
-//                     webBuilder.UseStartup<Startup>();
-//                 });
-//     }
-// }
